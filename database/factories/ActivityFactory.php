75105c880652ae6f4fb8e6832a663a6fe41e35d6
marcosrobvel <marcosraranjuez@gmail.com>
2025-06-01 13:46:42 +0200

    finishing-seeders-users-activities

---
+++ database/factories/ActivityFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Activity;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Activity>
+ */
+class ActivityFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'type' => $this->faker->randomElement(['surf', 'windsurf', 'kayak', 'atv', 'hot air balloon']),
+            'user_id' => User::factory(), // o puedes asignar un ID válido si ya existen usuarios
+            'datetime' => $this->faker->dateTimeBetween('now', '+1 month'),
+            'paid' => $this->faker->boolean(),
+            'notes' => $this->faker->optional()->paragraph,
+            'satisfaction' => $this->faker->optional()->numberBetween(0, 10),
+        ];
+    }
+}
